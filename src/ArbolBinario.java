81138a849e75468594ddd2d4a698b2b90284cad9
Andres <andresfelipeasprillaurrutia@gmail.com>
2023-06-09 21:10:28 -0500

    Andres branch v.1

---
+++ src/ArbolBinario.java
@@ -0,0 +1,138 @@
+
+public class ArbolBinario {
+    Nodo raiz;
+
+    public ArbolBinario() {
+        raiz = null;
+    }
+
+    public void insertar(int valor) {
+        raiz = insertarRecursivo(raiz, valor);
+    }
+
+    private Nodo insertarRecursivo(Nodo nodo, int valor) {
+        if (nodo == null) {
+            nodo = new Nodo(valor);
+            return nodo;
+        }
+
+        if (valor < nodo.valor) {
+            nodo.izquierdo = insertarRecursivo(nodo.izquierdo, valor);
+        } else if (valor > nodo.valor) {
+            nodo.derecho = insertarRecursivo(nodo.derecho, valor);
+        }
+
+        return nodo;
+    }
+
+    public boolean buscar(int valor) {
+        return buscarRecursivo(raiz, valor);
+    }
+
+    private boolean buscarRecursivo(Nodo nodo, int valor) {
+        if (nodo == null) {
+            return false;
+        }
+
+        if (valor == nodo.valor) {
+            return true;
+        } else if (valor < nodo.valor) {
+            return buscarRecursivo(nodo.izquierdo, valor);
+        } else {
+            return buscarRecursivo(nodo.derecho, valor);
+        }
+    }
+
+    public void editar(int valorAntiguo, int valorNuevo) {
+        raiz = editarRecursivo(raiz, valorAntiguo, valorNuevo);
+    }
+
+    private Nodo editarRecursivo(Nodo nodo, int valorAntiguo, int valorNuevo) {
+        if (nodo == null) {
+            return null;
+        }
+
+        if (valorAntiguo == nodo.valor) {
+            nodo.valor = valorNuevo;
+        } else if (valorAntiguo < nodo.valor) {
+            nodo.izquierdo = editarRecursivo(nodo.izquierdo, valorAntiguo, valorNuevo);
+        } else {
+            nodo.derecho = editarRecursivo(nodo.derecho, valorAntiguo, valorNuevo);
+        }
+
+        return nodo;
+    }
+
+    public void eliminar(int valor) {
+        raiz = eliminarRecursivo(raiz, valor);
+    }
+
+    private Nodo eliminarRecursivo(Nodo nodo, int valor) {
+        if (nodo == null) {
+            return null;
+        }
+
+        if (valor < nodo.valor) {
+            nodo.izquierdo = eliminarRecursivo(nodo.izquierdo, valor);
+        } else if (valor > nodo.valor) {
+            nodo.derecho = eliminarRecursivo(nodo.derecho, valor);
+        } else {
+            // Caso 1: Nodo sin hijos
+            if (nodo.izquierdo == null && nodo.derecho == null) {
+                nodo = null;
+            }
+            // Caso 2: Nodo con un hijo
+            else if (nodo.izquierdo == null) {
+                nodo = nodo.derecho;
+            } else if (nodo.derecho == null) {
+                nodo = nodo.izquierdo;
+            }
+            // Caso 3: Nodo con dos hijos
+            else {
+                Nodo sucesor = encontrarSucesor(nodo.derecho);
+                nodo.valor = sucesor.valor;
+                nodo.derecho = eliminarRecursivo(nodo.derecho, sucesor.valor);
+            }
+        }
+
+        return nodo;
+    }
+
+    private Nodo encontrarSucesor(Nodo nodo) {
+        while (nodo.izquierdo != null) {
+            nodo = nodo.izquierdo;
+        }
+        return nodo;
+    }
+
+    public void imprimirEnOrden() {
+        imprimirEnOrdenRecursivo(raiz);
+    }
+
+    private void imprimirEnOrdenRecursivo(Nodo nodo) {
+        if (nodo != null) {
+            imprimirEnOrdenRecursivo(nodo.izquierdo);
+            System.out.print(nodo.valor + " ");
+            imprimirEnOrdenRecursivo(nodo.derecho);
+        }
+    }
+
+    public int tamaño() {
+        return tamañoRecursivo(raiz);
+    }
+
+    private int tamañoRecursivo(Nodo nodo) {
+        if (nodo == null) {
+            return 0;
+        }
+
+        int tamañoIzquierdo = tamañoRecursivo(nodo.izquierdo);
+        int tamañoDerecho = tamañoRecursivo(nodo.derecho);
+
+        return tamañoIzquierdo + tamañoDerecho + 1;
+    }
+
+    public boolean estaVacio() {
+        return raiz == null;
+    }
+}
